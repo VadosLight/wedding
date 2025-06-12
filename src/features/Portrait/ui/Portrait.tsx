310f65175119bffc7759be407a185da30d427618
Vadim Tatarintsev <v.tatarincev@astondevs.ru>
2025-06-12 20:10:38 +0300

    add photo

--- src/features/Portrait/ui/Portrait.tsx
+++ src/features/Portrait/ui/Portrait.tsx
@@ -1,19 +1,20 @@
 import type { FC } from 'react'
 import styles from './Portrait.module.css'
+import portrait from './portrait.jpg'
 
 export const Portrait: FC = () => {
   return (
     <section className={styles.portrait}>
       <div className={styles.imageContainer}>
         <img 
-          src="/couple-portrait.jpg" 
+          src={portrait} 
           alt="Портрет пары" 
           className={styles.image}
         />
       </div>
       <div className={styles.textContent}>
         <h1 className={styles.names}>Кристина & Вадим</h1>
-        <p className={styles.date}>20.08.2024</p>
+        <p className={styles.date}>15.09.2025</p>
       </div>
     </section>
   )
